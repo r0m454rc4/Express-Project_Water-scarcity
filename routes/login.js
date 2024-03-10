fbd902e7dfeee7058a994a714e80ad517ed7e43f
romasarca <w9tdango98osaw@proton.me>
2024-03-10 18:09:57 +0000

    v.5 - Small improvements on pug interface.

--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
-
-// router.get("/", function (req, res, next) {
-//   res.sendFile(path.join(__dirname + "/public", "login.html"));
-// });
 
 router.get("/", function (req, res, next) {
   res.render("formulari", {
-    titol: "suma",
+    titol: "Login",
   });
 });
 
