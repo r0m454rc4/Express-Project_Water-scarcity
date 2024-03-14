b553afcb79a9dee04403b66dd25ca1a7f2a090df
romasarca <w9tdango98osaw@proton.me>
2024-03-14 12:28:58 +0000

    v.7 - Login connected to mongodb using monk.

--- routes/registre_usuari.js
+++ routes/registre_usuari.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.render("login", {
-    titol: "Login",
+  res.render("registre_usuari", {
+    titol: "Registrar usuari",
   });
 });
 
