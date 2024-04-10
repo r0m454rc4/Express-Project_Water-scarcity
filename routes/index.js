ef4260ab85b81f309b55c6a86099876678f8d73a
romasarca <w9tdango98osaw@proton.me>
2024-04-10 16:57:18 +0000

    v.8 - Trasform html to pug.

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,28 @@ const router = express.Router();
 const path = require("path");
 
 router.get("/", function (req, res, next) {
-  res.sendFile(path.join(__dirname + "/public", "index.html"));
+  // res.sendFile(path.join(__dirname + "/public", "index.html"));
+  res.render("index");
 });
 
 router.get("/nosotros", function (req, res, next) {
-  res.sendFile(path.join(__dirname + "/public", "nosotros.html"));
+  // res.sendFile(path.join(__dirname + "/public", "nosotros.html"));
+  res.render("nosotros");
 });
 
 router.get("/noticias", function (req, res, next) {
-  res.sendFile(path.join(__dirname + "/public", "noticias.html"));
+  // res.sendFile(path.join(__dirname + "/public", "noticias.html"));
+  res.render("noticias");
 });
 
 router.get("/contacto", function (req, res, next) {
-  res.sendFile(path.join(__dirname + "/public", "contacto.html"));
+  // res.sendFile(path.join(__dirname + "/public", "contacto.html"));
+  res.render("contacto");
 });
 
 router.get("/admin", function (req, res, next) {
-  res.sendFile(path.join(__dirname + "/public", "admin.html"));
+  // res.sendFile(path.join(__dirname + "/public", "admin.html"));
+  res.render("admin");
 });
 
 module.exports = router;
